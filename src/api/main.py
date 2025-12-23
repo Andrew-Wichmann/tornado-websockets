6cf9283fb863927517e3ee2ea8e4bba3e1ecee3c
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-12-23 00:03:49 -0500

    fix: fix rename

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,10 @@
-from api.app import Websockets
+from api.app import JobApplication
 from api.request_handlers.job import JobHandler
 import tornado
 
 
 if __name__ == "__main__":
-    app = Websockets(((r'/jobs/?', JobHandler),(r'/jobs/(\d+)/?', JobHandler)),)
+    app = JobApplication(((r'/jobs/?', JobHandler),(r'/jobs/(\d+)/?', JobHandler)),)
     app.listen(5150)
     tornado.ioloop.IOLoop.current().start()
 
